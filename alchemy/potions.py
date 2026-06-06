616f1004207f3e0e83d17b9c7ce71e5ef4bc701d
Hayuma Fujisada <fjsd6070@gmail.com>
2026-06-06 14:56:27 +0900

    aasasa

--- alchemy/potions.py
+++ alchemy/potions.py
@@ -11,4 +11,4 @@ def healing_potion() -> str:
 def strength_potion() -> str:
     fire = create_fire()
     water = create_water()
-    return f"Strength potion brewed with '{fire}'and '{water}'"
+    return f"Strength potion brewed with '{fire}' and '{water}'"
